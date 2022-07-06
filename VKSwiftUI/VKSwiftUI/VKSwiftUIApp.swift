aa94dd6f1d81fbae9d52108b2d331bdceac10dae
Stanislav96RUS <107839596+Stanislav96RUS@users.noreply.github.com>
2022-07-06 17:47:30 +0500

    New

---
+++ VKSwiftUI/VKSwiftUI/VKSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VKSwiftUIApp.swift
+//  VKSwiftUI
+//
+//  Created by Stanislav on 06.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct VKSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
