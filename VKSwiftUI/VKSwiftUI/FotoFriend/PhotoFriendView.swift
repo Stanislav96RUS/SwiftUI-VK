1ea3af0ba794d3484085d542ee56adede1a93782
Stanislav96RUS <107839596+Stanislav96RUS@users.noreply.github.com>
2022-08-01 11:09:53 +0500

    DZ8

--- VKSwiftUI/VKSwiftUI/FotoFriend/PhotoFriendView.swift
+++ VKSwiftUI/VKSwiftUI/FotoFriend/PhotoFriendView.swift
@@ -14,32 +14,37 @@ struct PhotosFriendView: View {
     
     @ObservedObject var viewModel: PhotoViewModel
     
+    
+    let columns = [
+    GridItem(.flexible(minimum: 0, maximum: .infinity)),
+    GridItem(.flexible(minimum: 0, maximum: .infinity)),
+    GridItem(.flexible(minimum: 0, maximum: .infinity)),]
+
+    
     init(viewModel: PhotoViewModel, id: Int) {
         self.id = id
         self.viewModel = viewModel
         viewModel.fetch()
-        
-
     }
     
     var body: some View {
-        
+        GeometryReader { geometry in
             ScrollView(.vertical) {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100, maximum: 300))]) {
+                LazyVGrid(columns: columns, alignment: .center, spacing: 16) {
                     ForEach(0..<viewModel.photos.count, id: \.self) { index in
                         KFImage(URL(string: viewModel.photos[index].photoName))
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 100, alignment: .leading)
+                            .frame(height: geometry.size.width/2)
                             .clipped()
                             .overlay(alignment: .bottom){
-                                LikesUI()}
+                            LikesUI()}
+                        }
+                        
                     }
                 }
-              
-                .onAppear(perform: viewModel.fetch)
+                
             }
-        }
+        .onAppear(perform: viewModel.fetch)
+    }
 }
 
 struct PhotosFriendView_Previews: PreviewProvider {
